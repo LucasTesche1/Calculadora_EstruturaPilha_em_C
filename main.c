c5944642f590c0fcc46478b0c95c1554c405dff5
LucasTesche1 <111753769+LucasTesche1@users.noreply.github.com>
2023-11-29 00:25:22 -0300

    Update main.c
    Implementação de função para converter

--- main.c
+++ main.c
@@ -1,164 +1,245 @@
-    #include <stdio.h>
-    #include <stdlib.h>
-    #include <ctype.h>
-    #include <math.h>
-    #include <string.h>
-    
-    #ifndef M_PI
-    #define M_PI (3.14159265358979323846)
-    #endif
-    typedef struct No
-    {
-        double dado;
-        struct No *proximo;
-    } No;
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+#include <string.h>
 
-    typedef struct
-    {
-        No *topo;
-    } Pilha;
+#define MAX_SIZE 100
 
-    void inicializar(Pilha *pilha)
+typedef struct No
+{
+    double dado;
+    struct No *proximo;
+} No;
+
+typedef struct
+{
+    No *topo;
+} Pilha;
+
+typedef struct
+{
+    char items[MAX_SIZE][100];
+    int top;
+} Stack;
+
+void inicializar(Pilha *pilha)
+{
+    pilha->topo = NULL;
+}
+
+void empilhar(Pilha *pilha, double valor)
+{
+    No *novoNo = (No *)malloc(sizeof(No));
+    if (novoNo == NULL)
     {
-        pilha->topo = NULL;
+        printf("Erro: Falha na alocacao de memória\n");
+        exit(EXIT_FAILURE);
     }
+    novoNo->dado = valor;
+    novoNo->proximo = pilha->topo;
+    pilha->topo = novoNo;
+}
 
-    void empilhar(Pilha *pilha, double valor)
+double desempilhar(Pilha *pilha)
+{
+    if (pilha->topo != NULL)
     {
-        No *novoNo = (No *)malloc(sizeof(No));
-        if (novoNo == NULL)
-        {
-            printf("Erro: Falha na alocacao de memória\n");
-            exit(EXIT_FAILURE);
-        }
-        novoNo->dado = valor;
-        novoNo->proximo = pilha->topo;
-        pilha->topo = novoNo;
+        No *temp = pilha->topo;
+        double valor = temp->dado;
+        pilha->topo = temp->proximo;
+        free(temp);
+        return valor;
     }
-
-    double desempilhar(Pilha *pilha)
+    else
     {
-        if (pilha->topo != NULL)
+        printf("Erro: Pilha vazia\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+double sqrt_custom(double x)
+{
+    if (x >= 0.0)
+        return sqrt(x);
+    else
+    {
+        printf("Erro: Raiz de número negativo\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+double log_custom(double x)
+{
+    if (x > 0.0)
+        return log10(x);
+    else
+    {
+        printf("Erro: Logaritmo de número não positivo\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+// Funções relacionadas à pilha
+void initialize(Stack *stack)
+{
+    stack->top = -1;
+}
+
+int isEmpty(Stack *stack)
+{
+    return stack->top < 0;
+}
+
+void push(Stack *stack, char *item)
+{
+    if (stack->top == MAX_SIZE - 1)
+    {
+        printf("Erro: A pilha está cheia\n");
+        exit(EXIT_FAILURE);
+    }
+    strcpy(stack->items[++stack->top], item);
+}
+
+char *pop(Stack *stack)
+{
+    if (isEmpty(stack))
+    {
+        printf("Erro: A pilha está vazia\n");
+        exit(EXIT_FAILURE);
+    }
+    return stack->items[stack->top--];
+}
+
+// Função para converter notação pós-fixada para notação infixa
+void postfixToInfix(char *expression)
+{
+    Stack operandStack;
+    initialize(&operandStack);
+
+    char *token = strtok(expression, " ");
+
+    while (token != NULL)
+    {
+        if (isdigit(token[0]) || (token[0] == '-' && isdigit(token[1])))
         {
-            No *temp = pilha->topo;
-            double valor = temp->dado;
-            pilha->topo = temp->proximo;
-            free(temp);
-            return valor;
+            push(&operandStack, token);
         }
         else
         {
-            printf("Erro: Pilha vazia\n");
-            exit(EXIT_FAILURE);
-        }
-    }
+            char operand2[100], operand1[100];
+            strcpy(operand2, pop(&operandStack));
+            strcpy(operand1, pop(&operandStack));
 
-    double sqrt_custom(double x) {
-        if (x >= 0.0)
-            return sqrt(x);
-        else {
-            printf("Erro: Raiz de número negativo\n");
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    double log_custom(double x) {
-        if (x > 0.0)
-            return log10(x);
-        else {
-            printf("Erro: Logaritmo de número não positivo\n");
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    double avaliarExpressaoPosfixada(const char *expressao)
-    {
-        Pilha pilha;
-        inicializar(&pilha);
-        char *token = strtok((char *)expressao, " ");
-        while (token != NULL)
-        {
-            if (isdigit(token[0]))
+            char temp[100];
+            if (strcmp(token, "cos") == 0)
             {
-                empilhar(&pilha, atof(token));
+                sprintf(temp, "%s(%s)", token, operand2);
             }
             else
             {
-                double operando1, operando2;
-                switch (token[0])
-                {
-                case '+':
-                    operando2 = desempilhar(&pilha);
-                    operando1 = desempilhar(&pilha);
-                    empilhar(&pilha, operando1 + operando2);
-                    break;
-                case '-':
-                    operando2 = desempilhar(&pilha);
-                    operando1 = desempilhar(&pilha);
-                    empilhar(&pilha, operando1 - operando2);
-                    break;
-                case '*':
-                    operando2 = desempilhar(&pilha);
-                    operando1 = desempilhar(&pilha);
-                    empilhar(&pilha, operando1 * operando2);
-                    break;
-                case '/':
-                    operando2 = desempilhar(&pilha);
-                    operando1 = desempilhar(&pilha);
-                    if (operando2 != 0.0)
-                    {
-                        empilhar(&pilha, operando1 / operando2);
-                    }
-                    else
-                    {
-                        printf("Erro: Divisao por zero\n");
-                        exit(EXIT_FAILURE);
-                    }
-                    break;
-                case '^':
-                    operando1 = desempilhar(&pilha);
-                    operando2 = desempilhar(&pilha);
-                    empilhar(&pilha, pow(operando1, operando2));
-                    break;
-                case 'r':
-                    operando1 = desempilhar(&pilha);
-                    empilhar(&pilha, sqrt_custom(operando1));
-                    break;
-                case 'l':
-                    operando1 = desempilhar(&pilha);
-                    empilhar(&pilha, log_custom(operando1));
-                    break;
-                case 's':
-                    operando1 = desempilhar(&pilha);
-                    empilhar(&pilha, sin(operando1)); 
-                break;
-                case 'c':
-                    operando1 = desempilhar(&pilha);
-                    empilhar(&pilha, cos(operando1));
-                    break;
-                case 't':
-                    operando1 = desempilhar(&pilha);                
-                    empilhar(&pilha, tan(operando1));  
-                    break;
-                default:
-                    printf("Erro: Operador '%c' invalido\n", token[0]);
-                    exit(EXIT_FAILURE);
-                }
+                sprintf(temp, "(%s %s %s)", operand1, token, operand2);
             }
-            token = strtok(NULL, " ");
+
+            push(&operandStack, temp);
         }
 
-        return desempilhar(&pilha);
+        token = strtok(NULL, " ");
     }
 
-void inverterString(char *str) {
+    printf("Expressão Infixa: %s\n", operandStack.items[0]);
+}
+
+double avaliarExpressaoPosfixada(const char *expressao)
+{
+    Pilha pilha;
+    inicializar(&pilha);
+    char *token = strtok((char *)expressao, " ");
+    while (token != NULL)
+    {
+        if (isdigit(token[0]))
+        {
+            empilhar(&pilha, atof(token));
+        }
+        else
+        {
+            double operando1, operando2;
+            switch (token[0])
+            {
+            case '+':
+                operando2 = desempilhar(&pilha);
+                operando1 = desempilhar(&pilha);
+                empilhar(&pilha, operando1 + operando2);
+                break;
+            case '-':
+                operando2 = desempilhar(&pilha);
+                operando1 = desempilhar(&pilha);
+                empilhar(&pilha, operando1 - operando2);
+                break;
+            case '*':
+                operando2 = desempilhar(&pilha);
+                operando1 = desempilhar(&pilha);
+                empilhar(&pilha, operando1 * operando2);
+                break;
+            case '/':
+                operando2 = desempilhar(&pilha);
+                operando1 = desempilhar(&pilha);
+                if (operando2 != 0.0)
+                {
+                    empilhar(&pilha, operando1 / operando2);
+                }
+                else
+                {
+                    printf("Erro: Divisao por zero\n");
+                    exit(EXIT_FAILURE);
+                }
+                break;
+            case '^':
+                operando1 = desempilhar(&pilha);
+                operando2 = desempilhar(&pilha);
+                empilhar(&pilha, pow(operando1, operando2));
+                break;
+            case 'r':
+                operando1 = desempilhar(&pilha);
+                empilhar(&pilha, sqrt_custom(operando1));
+                break;
+            case 'l':
+                operando1 = desempilhar(&pilha);
+                empilhar(&pilha, log_custom(operando1));
+                break;
+            case 's':
+                operando1 = desempilhar(&pilha);
+                empilhar(&pilha, sin(operando1));
+                break;
+            case 'c':
+                operando1 = desempilhar(&pilha);
+                empilhar(&pilha, cos(operando1));
+                break;
+            case 't':
+                operando1 = desempilhar(&pilha);
+                empilhar(&pilha, tan(operando1));
+                break;
+            default:
+                printf("Erro: Operador '%c' invalido\n", token[0]);
+                exit(EXIT_FAILURE);
+            }
+        }
+        token = strtok(NULL, " ");
+    }
+
+    return desempilhar(&pilha);
+}
+
+void inverterString(char *str)
+{
     if (str == NULL)
         return;
     int length = strlen(str);
     int start = 0;
     int end = length - 1;
 
-    while (start < end) {
+    while (start < end)
+    {
         char temp = str[start];
         str[start] = str[end];
         str[end] = temp;
@@ -167,127 +248,57 @@ void inverterString(char *str) {
     }
 }
 
-    double avaliarExpressaoPrefixada(const char *expressao)
-    {
-        Pilha pilha;
-        inicializar(&pilha);
-        char *expressao_copy = strdup(expressao);
+int main()
+{
+    const char *expressaoPosfixada = "3 4 + 5 tan *";
+    char *expressaoPosfixada_copy = strdup(expressaoPosfixada);
+    double resultadoPosfixado = avaliarExpressaoPosfixada(expressaoPosfixada_copy);
 
-        inverterString(expressao_copy);
+    printf("Resultado pos fixado: %.3f\n", resultadoPosfixado);
 
-        char *token = strtok(expressao_copy, " ");
-        while (token != NULL)
-        {
-            if (isdigit(token[0]))
-            {
-                empilhar(&pilha, atof(token));
-            }
-            else
-            {
-                double operando1, operando2;
-                switch (token[0])
-                {
-                case '+':
-                    operando2 = desempilhar(&pilha);
-                    operando1 = desempilhar(&pilha);
-                    empilhar(&pilha, operando1 + operando2);
-                    break;
-                case '-':
-                    operando2 = desempilhar(&pilha);
-                    operando1 = desempilhar(&pilha);
-                    empilhar(&pilha, operando1 - operando2);
-                    break;
-                case '*':
-                    operando2 = desempilhar(&pilha);
-                    operando1 = desempilhar(&pilha);
-                    empilhar(&pilha, operando1 * operando2);
-                    break;
-                case '/':
-                    operando2 = desempilhar(&pilha);
-                    operando1 = desempilhar(&pilha);
-                    if (operando2 != 0.0)
-                    {
-                        empilhar(&pilha, operando1 / operando2);
-                    }
-                    else
-                    {
-                        printf("Erro: Divisao por zero\n");
-                        exit(EXIT_FAILURE);
-                    }
-                    break;
-                case '^':
-                    operando1 = desempilhar(&pilha);
-                    operando2 = desempilhar(&pilha);
-                    empilhar(&pilha, pow(operando1, operando2));
-                    break;
-                case 'r':
-                    operando1 = desempilhar(&pilha);
-                    empilhar(&pilha, sqrt_custom(operando1));
-                    break;
-                case 'l':
-                    operando1 = desempilhar(&pilha);
-                    empilhar(&pilha, log_custom(operando1));
-                    break;
-                case 's':
-                    operando1 = desempilhar(&pilha);
-                    empilhar(&pilha, sin(operando1)); 
-                break;
-                case 'c':
-                    operando1 = desempilhar(&pilha);
-                    empilhar(&pilha, cos(operando1));
-                    break;
-                case 't':
-                    operando1 = desempilhar(&pilha);                
-                    empilhar(&pilha, tan(operando1));  
-                    break;
-                default:
-                    printf("Erro: Operador '%c' invalido\n", token[0]);
-                    exit(EXIT_FAILURE);
-                }
-            }
-            token = strtok(NULL, " ");
-        }
+    free(expressaoPosfixada_copy);
 
-        free(expressao_copy);
+    char expression[] = "3 4 + 5 tan *";
+    postfixToInfix(expression);
 
-        return desempilhar(&pilha);
-    }
+    return 0;
+}
 
-    int main()
-    {
-        const char *expressaoPosfixada = "45 60 + 30 cos *";
-        char *expressaoPosfixada_copy = strdup(expressaoPosfixada);
-        double resultadoPosfixado = avaliarExpressaoPosfixada(expressaoPosfixada_copy);
+/* RESULTADOS COM ÊXITO:
 
-        printf("Resultado pos fixado: %.3f\n", resultadoPosfixado);
+1 - 3 4 + 5 *
+    (3 + 4) * 5
+    35
 
-        free(expressaoPosfixada_copy);
+2 - 7 2 * 4 +
+    7 * 2 + 4
+    18
 
-        const char *expressaoPrefixada = "(3 + 4) * 5";
-        double resultadoPrefixado = avaliarExpressaoPrefixada(expressaoPrefixada);
+3 - 8 5 2 4 + * +
+    8 + (5 * (2 + 4))
+    38
 
-        printf("Resultado pre fixado: %f\n", resultadoPrefixado);
+4 - 6 2 / 3 + 4 *
+    (6 / 2 + 3) * 4
+    24
 
-        
+5 - 9 5 2 8 * 4 + * +
+    9 + (5 * (2 + 8 * 4))
+    109
 
-        return 0;
-    }
+6 - 2 3 + log 5 /
+    Aprox. 0.140
 
-    /*Este código agora suporta as seguintes operações:
-    - + para soma
-    - - para subtração
-    - * para multiplicação
-    - / para divisão
-    - ^ para potenciação
-    - r para radiciação
-    - l para logaritmo na base 10
-    - s para seno
-    - c para cosseno
-    - t para tangente
-    */
+7 - 10 log 3 ^ 2 +
+    5
 
-   /*
-   TESTES COM ÊXITO: 1,2,3,4,5,6,7,8,9,10
+8 - 45 60 + 30 cos *
+    Aprox. 16,20
 
-   TESTES COM PROBLEMAS:
-   */
+9 - 0.5 45 sen 2 ^ +
+    Aprox. 2,30
+
+10 - 3 4 + 5 tan *
+     Aprox. -23,66
+
+     */
